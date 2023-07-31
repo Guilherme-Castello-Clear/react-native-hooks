d99dae62e3a4fd2b847512c2bd24953d656e8f4e
Guilherme-Castello-Clear <guilherme.castello.clear@gmail.com>
2023-07-31 16:10:24 -0300

    Feat: Using useEffect

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React, {Fragment, useState} from 'react';
-
+import React, {Fragment, useState, useEffect } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { View, TextInput, Text, TouchableOpacity, StyleSheet } from 'react-native'
 
 export default function App(){
@@ -7,10 +7,31 @@ export default function App(){
   const [nome, setNome] = useState('Guilherme');
   const [input, setInput] = useState('')
 
+  useEffect(() => {
+    async function getStorage(){
+      const nomeStorage = await AsyncStorage.getItem('nome');
+      if(nomeStorage !== null) setNome(nomeStorage)
+    }
+
+
+  })
+
+  useEffect(() => {
+    async function saveStorage(){
+      await AsyncStorage.setItem('nome', nome)
+    }
+    saveStorage
+  }, [nome])
+
+  function alteraNome(){
+    setNome(input);
+    setInput('');
+  }
+
   return(
     <View>
       <Text>{nome}</Text>
-      <TouchableOpacity onPress={() => setNome(input)} style={{backgroundColor: 'green'}}>
+      <TouchableOpacity onPress={() => alteraNome()} style={{backgroundColor: 'green'}}>
         <Text>Alterar Nome</Text>
       </TouchableOpacity>
       <TextInput placeholder={'nome'} value={input} onChangeText={(texto) => setInput(texto)}/>
